82b3ed4cecaa627643c402718ea483a994350d82
high_templar <Gene_Work@163.com>
2022-08-27 09:42:32 +0800

    Added 'the Merge' function.

--- process.bat
+++ process.bat
@@ -2,8 +2,10 @@
 echo 编译中...
 g++ csvs_marks.cpp -o csvs_marks
 g++ marks_final.cpp -o marks_final
+g++ merge.cpp -o merge
 mkdir result
-del final.csv
+del "./result/final.csv"
+del "./result/merge.csv"
 echo 预处理完成
 pause
 for /l %%i in (1,1,10) do (
@@ -14,6 +16,13 @@ pause
 for /l %%i in (1,1,10) do (
 	marks_final.exe < "./result/%%i.csv"
 )
-echo 结束
+echo 总分计算完成
+pause
+for /l %%i in (1,1,10) do (
+	merge.exe < "./result/%%i.csv"
+)
+echo 合并完成
 move final.csv "./result/final.csv"
+move MERGE.csv "./result/MERGE.csv"
+echo 已结束
 pause
